53509d2d7834414037cc6f0bab575cfbfb1be83d
Jonathan Disla <jdisla2@mail.valenciacollege.edu>
2016-04-13 22:50:47 -0400

    Images6

--- test_images.py
+++ test_images.py
@@ -6,7 +6,7 @@ from PIL import Image
 def testDirectories():
     initialDirectory = os.getcwd()
     os.chdir('withLogo')
-    assert (os.getcwd() == os.path.join(initialDirectory,'withLogo'))
+    assert (os.getcwd() == os.path.join(initialDirectory,'../withLogo'))
 
 def testTotalFiles():
     os.chdir('../')
